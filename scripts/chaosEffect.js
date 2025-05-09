5fa7acd56f0cc299aca639d8fd16f151108c2de4
zorglemeister <richards.j.f@gmail.com>
2025-05-09 13:29:38 -0700

    adding makeGame function (in debug mode, currently)

--- scripts/chaosEffect.js
+++ scripts/chaosEffect.js
@@ -1,4 +1,11 @@
 
+// onLoad behaviours
+window.onload = setGameControlVisibility()
+
+//
+//
+// THIS IS THE GAME CONFIGURATION STUFF
+
 // Controls settings visibility
 function setSettingsVisibility() {
     var settingsDiv = document.getElementById("gameSettings");
@@ -10,6 +17,14 @@ function setSettingsVisibility() {
         btnText.innerHTML = "Hide Game Settings"; 
         settingsDiv.style.display = "block";
     }
+}
+// Control help text visibility
+function toggleHelp(targetSpan, triggerIcon) {
+    // var helpSpan = document.getElementById(toString(triggerIcon) + "Text");
+    var helpSpan = document.getElementById(targetSpan);
+    triggerIcon.classList.toggle('helpIconClosed'); // toggle Closed Book class
+    triggerIcon.classList.toggle('helpIconOpen'); // toggle Open Book class
+    helpSpan.classList.toggle('hiddenHelp'); // toggle Help visibility
 }
 // Updates player count button and clarification text
 function switchPlayerCount() {
@@ -215,6 +230,78 @@ function doItAll(inputElement) {
     encodeArrayToBitwise();
     decodeArrayFromBitwise();
 }
+// Collects all the game settings and turns them into an object
+// WHY DOESN'T THIS WORK??!?!?!?!
+function makeGame() {
+    // list configuration
+    // playerCount
+    var playerCount = document.getElementById("playerCountButton").innerHTML;
+    // baseList
+    var baseList = document.getElementById("listBase").value;
+    // physical
+    var physical = document.getElementById("physicalToggle").checked;
+    // exemplarTheme
+    var exemplarTheme = "";
+    if (document.getElementById("allThemes").checked === true) { // if it's "all", set it to "all"
+        exemplarTheme = "All";
+    } else {
+        exemplarTheme = document.getElementById("exemplarTheme").value; // otherwise set it to the selected single value
+    }
+    // customSchool
+    var schoolArray = [];
+    var schoolSelection = document.getElementById("customSchool");
+    for (let i = 0; i < schoolSelection.options.length; i++) {
+        if (schoolSelection.options[i].selected) {
+            schoolArray.push(schoolSelection.options[i].value);
+        }   
+    }
+    // customDuration
+    var durationArray = [];
+    var durationSelection = document.getElementById("customDuration");
+    for (let i = 0; i < durationSelection.options.length; i++) {
+        if (durationSelection.options[i].selected) {
+        durationArray.push(durationSelection.options[i].value);
+        }
+    }
+    // customRarity
+    var rarityArray = [];
+    var raritySelection = document.getElementById("customRarity");
+    for (let i = 0; i < raritySelection.options.length; i++) {
+        if (raritySelection.options[i].selected) {
+        rarityArray.push(raritySelection.options[i].value);
+        }
+    }
+    // randomizer controls
+    // repetition
+    var repetition = document.getElementById("repetitionToggle").checked;
+    // customRarityMatters
+    var customRarityMatters = document.getElementById("customRarityMAttersToggle").checked;
+    // additional functions
+    // vengeance
+    var vengeance = document.getElementById("vengeanceToggle").checked;
+    // minigames
+    var minigames = document.getElementById("minigameToggle").checked;
+    // minigameDelay
+    var minigameDelay = document.getElementById("minigameDelay").value;
+    //DEBUG OUTPUT
+    document.getElementById("debugPlayerCount").textContent = playerCount;
+    document.getElementById("debugBaseList").textContent = baseList;
+    document.getElementById("debugPhysical").textContent = physical.toString();
+    document.getElementById("debugExemplarTheme").textContent = exemplarTheme;
+    document.getElementById("debugCustomSchool").textContent = schoolArray.join(", ");
+    document.getElementById("debugCustomDuration").textContent = durationArray.join(", ");
+    document.getElementById("debugCustomRarity").textContent = rarityArray.join(", ");
+    document.getElementById("debugRepetition").textContent = repetition.toString();
+    document.getElementById("debugRarityMatters").textContent = customRarityMatters.toString();
+    document.getElementById("debugVengeance").textContent = vengeance.toString();
+    document.getElementById("debugMinigames").textContent = minigames.toString();
+    document.getElementById("debugMinigamesDelay").textContent = minigameDelay;
+}
+
+
+//
+//
+// THIS IS THE RANDOMIZER STUFF
 
 // Basic Randomizer
 // const sourceList = 
@@ -222,3 +309,32 @@ function doItAll(inputElement) {
 //     var 
 // }
 
+// random direction
+
+
+// random mana
+
+
+// random land type
+
+
+// random colour (and colourless)
+
+// die roller (count, sides)
+
+function dieRoll(count, sides) {
+    let total = 0;
+    for (let i = 0; i < count; i++){
+        let roll = Math.ceil(Math.random() * sides);
+        total = total + roll;
+    }
+    return total;
+}
+
+//
+//
+// THIS IS ALL THE EFFECT-LEVEL SPECIAL FUNCTIONS
+//
+// They should all return two strings:
+// one for the short desc
+// one for the full desc
