4b188f6be988554fa0037007761ee57f7e607c2b
zorglemeister <richards.j.f@gmail.com>
2025-05-14 15:11:59 -0700

    built filterCriteria object logic

--- scripts/chaosEffect.js
+++ scripts/chaosEffect.js
@@ -258,43 +258,111 @@ async function loadJSON(filePath) {
 // set up filterCriteria
 const filterCriteria = {
     player: "",
-    inclusion: "",
+    inclusion: [""],
     exemplarTheme: "",
     school: [""],
     duration: [""],
     rarity: [""]
 }
-// define the filter criteria (based on gameConfig array)
-function defineFilter() {
-    // First, the list selection will determine SO MUCH, so it's top level logic
+// define the filter criteria (based on gameConfig object)
+function defineMainFilter() {
+    
+    // Physical is set here
+    if (gameConfig.physical === "physical") {
+        filterCriteria.accessible = []; // if it's empty, everything is included
+    } else {
+        filterCriteria.accessible = [true]; // set to true, inaccessible effects will be excluded
+    }
+
+    // Player is set here
+        if (gameConfig.players === "multi") {
+        filterCriteria.player = ""; // if it's empty, everything is included
+    } else {
+        filterCriteria.player = "duel"; // set to duel, multi effects will be excluded
+    }
+
+    // the list selection will determine SO MUCH, so it's top level logic
     // aaaaaand I'm gonna do it with a case...
     // OH! Since I objectified gameConfig, and the filter condition is an object
     // I don't have to do this in a crazy tree!
 
-    // How... How do I add attributes to an object?
-    // can I do const filterCriteria.list = micro ??
     switch (gameConfig.list) {
         case "micro":
-            const filterCriteria.baseList = "micro"
-
+            filterMicro();
         break;
         case "lite":
-
+            filterLite();
         break;
         case "exemplar":
-
+            filterExemplar();
         break;
         case "legacy":
-
+            filterLegacy();
         break;
         case "full":
-
+            filterFull();
         break;
         case "custom":
-
+            filterCustom();
     }
 
 }
+
+// I'm going to put the filter construction for each list selection into it's own function
+function filterMicro() {
+    // Micro only cares about inclusion
+    filterCriteria.inclusion = gameConfig.list;
+    filterCriteria.exemplarTheme = "";
+    filterCriteria.school = [""];
+    filterCriteria.duration = [""];
+    filterCriteria.rarity = [""];
+}
+function filterLite() {
+    // Lite  only cares about inclusion
+    filterCriteria.inclusion = gameConfig.list;
+    filterCriteria.exemplarTheme = "";
+    filterCriteria.school = [""];
+    filterCriteria.duration = [""];
+    filterCriteria.rarity = [""];
+}
+function filterExemplar() {
+    // Exemplar cares about inclusion and theme (if all, include everything)
+    filterCriteria.inclusion = gameConfig.list;
+    if (gameConfig.theme === "all") {
+        filterCriteria.exemplarTheme = ["tokenizer", "complexity", "soundalike", "prodigal", "beat", "university", "toxin", "badthings"];
+    } else {
+        filterCriteria.exemplarTheme = gameConfig.theme;
+    }
+    filterCriteria.school = [""];
+    filterCriteria.duration = [""];
+    filterCriteria.rarity = [""];
+}
+function filterLegacy() {
+    // Legacy only cares about inclusion
+    filterCriteria.inclusion = gameConfig.list;
+    filterCriteria.exemplarTheme = "";
+    filterCriteria.school = [""];
+    filterCriteria.duration = [""];
+    filterCriteria.rarity = [""];
+}
+function filterFull() {
+    // Full REALLY doesn't care
+    filterCriteria.inclusion = "";
+    filterCriteria.exemplarTheme = "";
+    filterCriteria.school = [""];
+    filterCriteria.duration = [""];
+    filterCriteria.rarity = [""];
+}
+function filterCustom() {
+    // Custom cares about inclusion, school, duration, rarity
+    filterCriteria.inclusion = gameConfig.list;
+    filterCriteria.exemplarTheme = "";
+    filterCriteria.school = gameConfig.school;
+    filterCriteria.duration = gameConfig.duration;
+    filterCriteria.rarity = gameConfig.rarity;
+}
+
+
 
 
 
