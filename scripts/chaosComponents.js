63db6e91dd0d101e36113f6ccbe915589bb4657b
zorglemeister <richards.j.f@gmail.com>
2025-06-02 20:34:24 -0700

    added images, bunch of specFunc work, starting inline symbol component

--- scripts/chaosComponents.js
+++ scripts/chaosComponents.js
@@ -3,6 +3,38 @@
 
 class ChaosToggle extends HTMLElement {
     connectedCallback() {
-        
+
     }
+}
+
+// INLINE SYMBOLS!
+// Red = r = r.png
+// Black = b = b.png
+// Green = g = g.png
+// Blue = u = u.png
+// White = w = w.png
+// Tap = t = t.png
+// X = x = x.png
+// Infinite = i = i.png
+// Colourless = c = c.png
+// 0 = 0 = 0.png
+// 1 = 1 = 1.png
+// 2 = 2 = 2.png
+// 3 = 3 = 3.png
+// Energy = e = e.png
+
+// writing as a function first...
+function generateInlineSymbols(input) {
+    // where are the files?
+    const baseURL = "images/";
+    // lowercase for consistency
+    const lowercaseInput = input.toLowerCase();
+    // empty html
+    let html = "";
+    // loop through characters
+    for (let character of lowercaseInput) {
+        // append the image ref
+        html += '<img class="inlineSymbol" src="' += baseURL += character += '.png" alt="' += character =+ '">';
+    }
+    return html
 }
