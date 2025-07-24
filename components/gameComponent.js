29511ac0687c474de5409265d3ab12d7fb70bfec
zorglemeister <richards.j.f@gmail.com>
2025-07-23 20:50:52 -0700

    building our gamestate flows

--- components/gameComponent.js
+++ components/gameComponent.js
@@ -4,6 +4,32 @@
 
 // for now, it's notes.
 
+// Initial state:
+// active = empty
+// history = empty
+// roll = visible
+// vengeance = setting
+// minigame = setting
+
+// Main flow:
+// Roll is clicked
+// if activeEffect exists, move to history (visual note: slide historyDrawer up just enough to see the old effect add to it, then close it again)
+// generate newEffect, add to active
+// increment turn counter
+
+// vengeance flow:
+// vengeace is clicked
+// if vengeanceEffect exists, remove it from vengeanceModal
+// generate vengeanceEffect, add to vengeanceModal
+// display vengeanceModal
+// when "vengeance complete" is clicked, hide vengeanceModal
+
+// minigame flow
+// (pretty much just the same as vengeance)
+
+
+
+
 // here's moving the active effect to the top of the history section:
 moveActiveToHistory() {
     const activeEffect = document.getElementById('activeContainer').firstChild; // get the first (and only) div in the active container
